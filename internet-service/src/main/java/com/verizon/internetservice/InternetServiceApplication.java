5d3997f0c42187ef476dcbb2071e8191728678c1
cwsvensson <cws.commercial@gmail.com>
2021-05-04 19:08:44 -0400

    internet and cable services aligned to Consul

--- internet-service/src/main/java/com/verizon/internetservice/InternetServiceApplication.java
+++ internet-service/src/main/java/com/verizon/internetservice/InternetServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
 //import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //@EnableFeignClients
 @EnableDiscoveryClient
