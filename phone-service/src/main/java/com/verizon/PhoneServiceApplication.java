8f8643e48fff42610b5bc8afdfe208e028666d6d
cwsvensson <cws.commercial@gmail.com>
2021-05-04 19:26:25 -0400

    added phone service to Consul discovery

--- phone-service/src/main/java/com/verizon/PhoneServiceApplication.java
+++ phone-service/src/main/java/com/verizon/PhoneServiceApplication.java
@@ -2,13 +2,15 @@ package com.verizon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+//@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
-@EnableEurekaClient
 public class PhoneServiceApplication {
 
     public static void main(String[] args) { SpringApplication.run(PhoneServiceApplication.class, args);
     }
-
+    
 }
