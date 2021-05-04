c67cbb96b7ae113b6393bc364171fca9acb2a1ea
cwsvensson <cws.commercial@gmail.com>
2021-05-04 17:29:54 -0400

    internet-service to consul

--- internet-service/src/main/java/com/verizon/internetservice/InternetServiceApplication.java
+++ internet-service/src/main/java/com/verizon/internetservice/InternetServiceApplication.java
@@ -2,12 +2,15 @@ package com.verizon.internetservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
 //import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+//@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
-@EnableEurekaClient
 public class InternetServiceApplication {
 
 	public static void main(String[] args) {
