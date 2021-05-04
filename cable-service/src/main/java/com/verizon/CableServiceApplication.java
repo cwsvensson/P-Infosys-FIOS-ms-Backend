d4494be6290775c2609108ddc52e02eca1e6f8ca
ClayFlores <ClayFlores@users.noreply.github.com>
2021-05-04 14:27:54 -0700

    adds cable service discovery
    allows cable service to be discovered by consul

--- cable-service/src/main/java/com/verizon/CableServiceApplication.java
+++ cable-service/src/main/java/com/verizon/CableServiceApplication.java
@@ -2,10 +2,12 @@ package com.verizon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
+@EnableDiscoveryClient
 public class CableServiceApplication {
 
 	public static void main(String[] args) {
