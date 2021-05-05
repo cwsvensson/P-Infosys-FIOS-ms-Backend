24436432192fddbebc3b84562171845803c29c62
cwsvensson <cws.commercial@gmail.com>
2021-05-05 15:36:03 -0400

    added fios gateway microservice to Consul

--- fios-service/src/main/java/com/verizon/FiosServiceApplication.java
+++ fios-service/src/main/java/com/verizon/FiosServiceApplication.java
@@ -2,12 +2,13 @@ package com.verizon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
-@EnableEurekaClient
 public class FiosServiceApplication 
 {
 	public static void main(String[] args) 
